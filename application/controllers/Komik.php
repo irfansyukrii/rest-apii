912bcf21f0e108aa52a8a4f200f37c68a3b91872
irfansyukrii <irfansyukri6@gmail.com>
2020-07-28 19:12:21 +0700

    bara

---
+++ application/controllers/Komik.php
@@ -0,0 +1,32 @@
+ <?php
+    require_once APPPATH . '/libraries/REST_Controller.php';
+
+    class Komik extends REST_Controller
+    {
+        public function komik_get()
+        {
+            $data = $this->db->get('komik');
+            $this->response($data->result(), 200);
+        }
+        public function komik_post()
+        {
+            $data = $this->post();
+            $result = $this->db->insert('komik', $data);
+            $this->response($result, 200);
+        }
+        public function komik_put()
+        {
+            $data = $this->put();
+            $id = $this->uri->segment(3);
+            $this->db->where('id', $id);
+            $result = $this->db->update('komik', $data);
+            $this->response($result, 200);
+        }
+        public function komik_delete()
+        {
+            $id = $this->uri->segment(3);
+            $this->db->where('id', $id);
+            $result = $this->db->delete('komik');
+            $this->response($result, 200);
+        }
+    }
